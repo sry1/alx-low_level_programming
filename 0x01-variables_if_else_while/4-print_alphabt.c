c6680b3b21fe69f6fa132824c042399191da598a
sry1 <steve.roy99@yahoo.com>
2022-03-11 12:48:18 +0000

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main- Write a program that prints the alphabet in lowercase.
+ * main - program that prints the alphabet in lowercase, followed by a new line
  * You can only use the putchar
- * Return:
+ * Return: 0
  */
 int main(void)
 {
